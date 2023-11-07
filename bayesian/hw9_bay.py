1978797df735c27d5350f3a3814894d854d4f7f9
jonorgan <jonorgan26@gmail.com>
2023-11-07 12:58:15 -0500

    Finished question 1 of bayes

--- bayesian/hw9_bay.py
+++ bayesian/hw9_bay.py
@@ -8,10 +8,23 @@ from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("cmg_weeks.csv")
 
+# Question 1 ========================================================================================
+print("Question 1:")
+
 X = df[['Avg_Return', 'Volatility']][df['Week'] <= 50].values
 Y = df['Color'][df['Week'] <= 50].values
 
 NB_classifier = GaussianNB().fit(X, Y)
-prediction = NB_classifier.predict([[0.011720, 0.006433]])
-print(prediction)
 
+prediction = NB_classifier.predict(df[['Avg_Return', 'Volatility']][(df['Week'] > 50) & (df['Week'] <= 100)].values)
+actual = df['Color'][(df['Week'] > 50) & (df['Week'] <= 100)].values
+
+
+score = NB_classifier.score(df[['Avg_Return', 'Volatility']][(df['Week'] > 50) & (df['Week'] <= 100)].values, 
+	df['Color'][(df['Week'] > 50) & (df['Week'] <= 100)].values)
+print("Gaussian Naive Bayesian year 2 accuracy: " + str(score * 100) + "%")
+
+
+print("\n")
+# Question 2 ========================================================================================
+print("Question 2:")
