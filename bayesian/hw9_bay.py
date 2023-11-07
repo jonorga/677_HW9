5caef2cc912a2536cf3f1950272422e797da6ade
jonorgan <jonorgan26@gmail.com>
2023-11-07 12:47:56 -0500

    Figured out proper bayesian to use

--- bayesian/hw9_bay.py
+++ bayesian/hw9_bay.py
@@ -4,17 +4,14 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("cmg_weeks.csv")
 
-input_data = df[['Avg_Return', 'Volatility']][df['Week'] <= 50]
-dummies = [pd.get_dummies(df[c]) for c in input_data.columns]
-binary_data = pd.concat(dummies, axis=1)
-X = binary_data[0:50].values
-le = LabelEncoder()
-Y = le.fit_transform(df['Color'][df['Week'] <= 50].values)
-NB_classifier = MultinomialNB().fit(X, Y)
+X = df[['Avg_Return', 'Volatility']][df['Week'] <= 50].values
+Y = df['Color'][df['Week'] <= 50].values
 
+NB_classifier = GaussianNB().fit(X, Y)
+prediction = NB_classifier.predict([[0.011720, 0.006433]])
+print(prediction)
 
